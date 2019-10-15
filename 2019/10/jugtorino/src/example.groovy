56ee06b63f697f2d8a5acd7ad30dddbf04fb27c4
lburgazzoli <lburgazzoli@gmail.com>
2019-10-15 17:42:42 +0200

    code

--- 2019/10/jugtorino/src/example.groovy
+++ 2019/10/jugtorino/src/example.groovy
@@ -2,6 +2,7 @@
 //
 // kamel run --dev src/example.groovy
 //
-from('timer:example?period=1s')
-    .setBody().constant('hello')
-    .log('${body}')
+from('timer:groovy?period=1s')
+    .setBody()
+        .constant('Hello Camel K!')
+    .to('log:info?showAll=false')
